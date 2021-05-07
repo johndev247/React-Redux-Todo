fa8cf9712beac9589d725b42cb462819e5c3bbe1
johndev247 <johndev247@gmail.com>
2021-05-07 13:33:18 +0100

    todolist

--- src/Features/Todos/TodoSlice.js
+++ src/Features/Todos/TodoSlice.js
@@ -1,35 +1,8 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
 
-const Todos = [
-  {
-    id: nanoid(),
-    desc: "React Redux",
-    completed: false,
-  },
-  {
-    id: nanoid(),
-    desc: " Redux Toolkit",
-    completed: false,
-  },
-  {
-    id: nanoid(),
-    desc: "John Codes",
-    completed: true,
-  },
-  {
-    id: nanoid(),
-    desc: "Whats New?",
-    completed: true,
-  },
-  {
-    id: nanoid(),
-    desc: "React Redux Api",
-    completed: false,
-  },
-];
 export const TodoSlice = createSlice({
   name: "Todo",
-  initialState: Todos,
+  initialState: [],
   reducers: {
     AddTodos: (state, action) => {
       const newTodo = {
